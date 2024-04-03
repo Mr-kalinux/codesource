042253c95c4980aa2fb74d431e59d02201b4dbeb
Mr-kalinux <164866453+Mr-kalinux@users.noreply.github.com>
2024-04-03 11:22:39 +0200

    Add files via upload

---
+++ siteweb.js
@@ -0,0 +1,11 @@
+const logo = document.querySelector('.logo');
+window.addEventListener('load', () => {
+	    const TL = gsap.timeline({paused: true});
+
+	    TL
+        .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
+
+        TL.play();
+})        
+
+
